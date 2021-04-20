9ae6b1a3a37b426b707c498fde91bcd1fb53edc9
JustAndrej2510 <andrey.pisaryk@gmail.com>
2021-04-20 22:54:59 +0300

    Добавлен класс Node для хранения координат клетки

--- Rover.cs
+++ Rover.cs
@@ -8,6 +8,21 @@ namespace Rover
 {
     public class Rover
     {
+        public class Node
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Node() { }
+            public Node(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            
+
+        }
         public static void CalculateRoverPath(int[,] map)
         {
 
